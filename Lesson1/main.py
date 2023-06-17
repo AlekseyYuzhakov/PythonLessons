b32eba5b1d03d422699505e07c11011fae0dcc7d
Aleksey Yuzhakov <theval4a@gmail.com>
2023-06-17 12:16:34 +0500

    initial commit

---
+++ Lesson1/main.py
@@ -0,0 +1,28 @@
+# a = 5
+# print(type(a))
+# b = 5.89
+# print(type(b))
+# c = "Vasia"
+# print(type(c))
+
+# # print (type(n))
+
+# print(f"{a}")
+
+# print("{} - {} - {}".format(a, b, c))
+
+# print("Введите первое число")
+# n = int(input())
+# print(n)
+
+# print(a+b)
+
+# a = 5.8485
+# b = 56.444
+
+# print(round(a*b, 2))
+
+# i = 2
+# i +=3
+# print(i)
+
